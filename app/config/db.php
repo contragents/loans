4460477511efb8ff78951945c0ca5461271db72e
Ilya Cherkasov <ilia.cherkasov@cudev.org>
2025-11-09 16:59:15 +0300

    main update 5

--- app/config/db.php
+++ app/config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=db;port=5432;dbname=' . ($_ENV['POSTGRES_DB'] ?? 'loans'),
-    'username' => $_ENV['POSTGRES_USER'] ?? 'user',
-    'password' => $_ENV['POSTGRES_PASSWORD'] ?? 'password',
+    'dsn' => "pgsql:host=db;port=5432;dbname={$_ENV['POSTGRES_DB']}",
+    'username' => $_ENV['POSTGRES_USER'],
+    'password' => $_ENV['POSTGRES_PASSWORD'],
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
--- app/web/index.php
+++ app/web/index.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->safeLoad();
 
-defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV'] ?? 'prod');
+defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV']);
 defined('YII_DEBUG') or define('YII_DEBUG', YII_ENV !== 'prod');
 defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER', false);
 
